b31f23af7dad0913b180f0d2ac1a653897832a81
Anuj Kumar Chaudhary <117503109+akc2531@users.noreply.github.com>
2023-11-01 16:58:55 +0530

    Update Postfix_suffix_in_pointer.c

--- Postfix_suffix_in_pointer.c
+++ Postfix_suffix_in_pointer.c
@@ -5,8 +5,13 @@ int main() {
     // Write C code here
     //printf("Hello world");
       int val=100,*pt=&val;
-      printf("%u\n",pt);
+      printf("%u and %d\n",pt,*pt);
       ++pt;
-      printf("%u",pt);
+      printf("%u and %d",pt,*pt);
     //return 0;
 }
+
+/*O/p
+3267050388 and 100
+3267050392 and -1027916904
+    */
