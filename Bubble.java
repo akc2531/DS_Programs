ececd6e25af0c96bb66994a7eed5fa8692d34f06
Anuj Kumar Chaudhary <117503109+akc2531@users.noreply.github.com>
2023-11-01 13:55:40 +0530

    Create Bubble.java

---
+++ Bubble.java
@@ -0,0 +1,19 @@
+class buble_sort
+{
+    void main(String args[])
+    {
+        int arr[]={5,2,6,1};
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr.length-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int temp= arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }   
+}
+}
