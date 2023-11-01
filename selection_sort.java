3f6e528c58722437ed8cb13bf2c875a33614f8a6
Anuj Kumar Chaudhary <117503109+akc2531@users.noreply.github.com>
2023-11-01 13:57:58 +0530

    Create selection_sort.java

---
+++ selection_sort.java
@@ -0,0 +1,19 @@
+class selection_sort
+{
+    void main(String args[])
+    {
+        int arr[]={5,2,6,1};
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                int temp= arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    } 
+}
+}
