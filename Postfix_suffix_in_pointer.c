b8c956a302a3b1833ca8f4a6b66277700e68ed03
Anuj Kumar Chaudhary <117503109+akc2531@users.noreply.github.com>
2023-11-01 16:54:57 +0530

    Create Postfix_suffix_in_pointer.c

---
+++ Postfix_suffix_in_pointer.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    //printf("Hello world");
+      int val=100,*pt=&val;
+      printf("%u\n",pt);
+      ++pt;
+      printf("%u",pt);
+    //return 0;
+}
