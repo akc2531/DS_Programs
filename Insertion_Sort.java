737444857bbd8397422d1a05590d3122e906e6f8
Anuj Kumar Chaudhary <117503109+akc2531@users.noreply.github.com>
2024-02-22 14:24:29 +0530

    Create Insertion_Sort.java

---
+++ Insertion_Sort.java
@@ -0,0 +1,34 @@
+/*Ascending Order*/
+
+public static void insertionSort(int[] arr) {
+         
+    for(int i=1;i<arr.length;i++)
+      {
+        int key=arr[i];
+        int j=i-1;
+        while(j>=0&&arr[j]>key)
+          {
+            arr[j+1]=arr[j];
+            j--;
+          }
+        arr[j+1]=key;
+      }
+    return arr;
+}
+
+/*Descending Order*/
+public static void insertionSort(int[] arr) {
+         
+    for(int i=1;i<arr.length;i++)
+      {
+        int key=arr[i];
+        int j=i-1;
+        while(j>=0&&arr[j]<key)
+          {
+            arr[j+1]=arr[j];
+            j--;
+          }
+        arr[j+1]=key;
+      }
+    return arr;
+}
